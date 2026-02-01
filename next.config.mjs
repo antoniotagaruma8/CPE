ee774818c5ccaa214e5ad642cc4c018b2cc66032
agtagaruma <agtagaruma05@gmail.com>
2026-02-01 09:23:29 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,17 @@
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
